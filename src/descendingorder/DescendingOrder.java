3b13fb366edf7b7d6778d77e59f6dd01057d2148
tagger1986 <nostojic@hotmail.com>
2017-10-07 23:05:19 +0200

    Sort and descend integer

--- src/descendingorder/DescendingOrder.java
+++ src/descendingorder/DescendingOrder.java
@@ -9,13 +9,13 @@ public class DescendingOrder
     public static int sortDesc(final int num) 
     {
         String[] broj = (Integer.toString(num)).split("");
-        Arrays.sort(broj);
+        Arrays.sort(broj); //sortiranje niza koji je prebacen u String
         String result = "";
             for(String s : broj)
                 {
-                 result = s + result;
+                 result = s + result; // moye npr i preko StringBulidera
                 }
-        return Integer.parseInt(result);
+        return Integer.parseInt(result); //String parsiran ponovo u Integer
     }
 
     public static void main(String[] args) 
